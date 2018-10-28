429e4d92e274761e5a35ea47b7ad39c50df7ec29
TheIllusiveC4 <itwasmec4@gmail.com>
2018-10-28 01:45:36 -0700

    fix tinkers integration

--- src/main/java/c4/consecration/integrations/ModuleTConstruct.java
+++ src/main/java/c4/consecration/integrations/ModuleTConstruct.java
@@ -8,13 +8,18 @@
 
 package c4.consecration.integrations;
 
+import c4.consecration.common.util.UndeadHelper;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.entity.EntityProjectileBase;
+import slimeknights.tconstruct.library.materials.Material;
 import slimeknights.tconstruct.library.tinkering.TinkersItem;
 import slimeknights.tconstruct.library.utils.TagUtil;
 import slimeknights.tconstruct.library.utils.TinkerUtil;
@@ -23,6 +28,8 @@ import slimeknights.tconstruct.shared.block.BlockSoil;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 import slimeknights.tconstruct.tools.TinkerTools;
 import slimeknights.tconstruct.tools.TinkerTraits;
+
+import java.util.List;
 
 public class ModuleTConstruct extends ModuleCompatibility {
 
@@ -33,28 +40,21 @@ public class ModuleTConstruct extends ModuleCompatibility {
     @Override
     public boolean process(EntityLivingBase target, DamageSource source) {
 
-        if (TConstruct.pulseManager.isPulseLoaded(TinkerTools.PulseId)) {
+        ItemStack stack;
 
-            if (source.getImmediateSource() instanceof EntityLivingBase) {
+        if (source.getImmediateSource() instanceof EntityProjectileBase) {
+            stack = ((EntityProjectileBase) source.getImmediateSource()).tinkerProjectile.getItemStack();
 
-                ItemStack stack = ((EntityLivingBase) source.getImmediateSource()).getHeldItemMainhand();
+            if (hasHolyMaterial(TagUtil.getBaseMaterialsTagList(stack))) {
+                return true;
+            }
+        }
 
-                if (TConstruct.pulseManager.isPulseLoaded(TinkerTools.PulseId) && stack.getItem() instanceof TinkersItem) {
-                    if (TinkerUtil.hasTrait(TagUtil.getTagSafe(stack), TinkerTraits.holy.getIdentifier()) || TinkerUtil.hasModifier(TagUtil.getTagSafe(stack), TinkerModifiers.modSmite.getIdentifier())) {
-                        return true;
-                    }
-                }
+        if (source.getTrueSource() instanceof EntityLivingBase) {
+            stack = ((EntityLivingBase) source.getTrueSource()).getHeldItemMainhand();
 
-            } else {
-
-                if (source.getImmediateSource() instanceof EntityProjectileBase) {
-                    if (TConstruct.pulseManager.isPulseLoaded(TinkerTools.PulseId)) {
-                        ItemStack stack = ((EntityProjectileBase) source.getImmediateSource()).tinkerProjectile.getItemStack();
-                        if (TinkerUtil.hasTrait(TagUtil.getTagSafe(stack), TinkerTraits.holy.getIdentifier()) || TinkerUtil.hasModifier(TagUtil.getTagSafe(stack), TinkerModifiers.modSmite.getIdentifier())) {
-                            return true;
-                        }
-                    }
-                }
+            if (hasHolyMaterial(TagUtil.getBaseMaterialsTagList(stack))) {
+                return true;
             }
         }
 
@@ -63,7 +63,17 @@ public class ModuleTConstruct extends ModuleCompatibility {
         if (state.getBlock() == TinkerCommons.blockSoil && state.getValue(BlockSoil.TYPE) == BlockSoil.SoilTypes.CONSECRATED) {
             return true;
         }
+        return false;
+    }
 
+    private boolean hasHolyMaterial(NBTTagList materialTag) {
+
+        for (int i = 0; i < materialTag.tagCount(); i++) {
+
+            if (UndeadHelper.isHolyMaterial(materialTag.getStringTagAt(i))) {
+                return true;
+            }
+        }
         return false;
     }
 }
