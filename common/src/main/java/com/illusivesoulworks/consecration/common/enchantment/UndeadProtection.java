ca032aa678f57c683cf38560288425cf4c74dfdc
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2023-07-10 19:20:32 -0700

    Update copyright

--- common/src/main/java/com/illusivesoulworks/consecration/common/enchantment/UndeadProtection.java
+++ common/src/main/java/com/illusivesoulworks/consecration/common/enchantment/UndeadProtection.java
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2017-2020 C4
- *
- * This file is part of Consecration, a mod made for Minecraft.
+ * Copyright (C) 2017-2023 Illusive Soulworks
  *
  * Consecration is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * Consecration is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
--- common/src/main/java/com/illusivesoulworks/consecration/common/registry/RegistryObject.java
+++ common/src/main/java/com/illusivesoulworks/consecration/common/registry/RegistryObject.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017-2023 Illusive Soulworks
+ *
+ * Consecration is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Consecration is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Consecration.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.illusivesoulworks.consecration.common.registry;
 
 import java.util.function.Supplier;
